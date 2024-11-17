2cd50b6c78cbeece8a9a3e4d6f7f002d02b756d7
DanielAvilar <da.avilar@duocuc.cl>
2024-11-16 22:25:58 -0300

    Co-authored-by: slickDhole <slickDhole@users.noreply.github.com>

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { AuthService } from 'src/app/servicios/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  usuario: string = '';
+  clave: string = '';
 
-  ngOnInit() {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  private loginFailedSubject = new BehaviorSubject(false);
+  loginFailed$ = this.loginFailedSubject.asObservable();
+  loginFailed: boolean;
+
+  ngOnInit(): void {
+    this.authService.loginFailed$.subscribe(loginFailed => this.loginFailed = loginFailed);
+  }
+
+  constructor() {}
+
+  isLoading: boolean = false;
+  async login(usuario: string,clave: string){
+
+    this.isLoading = true;
+    await this.authService.buscarBD4(usuario,clave);
+    this.isLoading = false;
+
+    this.authService.isAuthenticated$.subscribe(isAuthenticated => {
+
+      this.authService.usuarioCompleto$.subscribe(usuarioCompleto => {
+        if (isAuthenticated) {
+          this.usuario = ''; 
+          this.clave = ''; 
+
+          if (usuarioCompleto != null && usuarioCompleto){
+            if (usuarioCompleto.rol === "vendedor") {
+              console.log('LOGIN: ' + usuarioCompleto.rol);
+              this.usuario = ''; 
+              this.clave = ''; 
+              this.router.navigate(['/vendedor']); 
+            }
+            else{
+              this.usuario = ''; 
+              this.clave = ''; 
+              this.router.navigate(['/cliente']); 
+            }
+          }
+
+        } else {
+          this.loginFailed = true; 
+        }
+
+      });
+
+    });
   }
 
 }
